3d9eb63a51cea077c889c1147bba8be1d81601e6
Damian <damian.ertel5@gmail.com>
2021-02-03 15:08:28 +0100

    bugfix

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
@@ -32,7 +33,7 @@ app.use('/admin', adminRouter)
 
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/frontend')))
+    app.use(express.static(path.join(__dirname, './frontend/build')))
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend','build','index.html'))
     )
 }
