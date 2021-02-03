1ed9c651433c1f38267ce5c8f96515abc56d6a4b
Damian <damian.ertel5@gmail.com>
2021-02-03 15:01:00 +0100

    Moving frontend to backend folder part 2

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use('/admin', adminRouter)
 
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.use(express.static(path.join(__dirname, '/frontend')))
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend','build','index.html'))
     )
 }
